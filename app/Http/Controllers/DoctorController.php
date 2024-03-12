0252c2dcc28b292e5f4a831cece8c2036349ba69
asepgunawanypd <asepgunawanypd@gmail.com>
2024-03-12 09:40:25 +0700

    first commit

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Doctor;
+use Illuminate\Support\Facades\DB;
+
+class DoctorController extends Controller
+{
+    public function index(Request $request)
+    {
+        $doctors = DB::table('doctors')
+            ->when($request->input('name'), function ($query, $doctor_name) {
+                return $query->where('doctor_name', 'like', '%' . $doctor_name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.doctors.index', compact('doctors'));
+    }
+
+    //create doctor
+    public function create()
+    {
+        return view('pages.doctors.create');
+    }
+
+    //store doctor
+    public function store(Request $request)
+    {
+        $request->validate([
+            'doctor_name' => 'required',
+            'doctor_specialist' => 'required',
+            'doctor_phone' => 'required',
+            'doctor_email' => 'required|email',
+            'sip' => 'required',
+        ]);
+
+        DB::table('doctors')->insert([
+            'doctor_name' => $request->doctor_name,
+            'doctor_specialist' => $request->doctor_specialist,
+            'doctor_phone' => $request->doctor_phone,
+            'doctor_email' => $request->doctor_email,
+            'sip' => $request->sip,
+        ]);
+
+        return redirect()->route('doctors.index')->with('success', 'Doctor created successfully');
+    }
+
+    //show doctor
+    public function show($id)
+    {
+        $doctor = DB::table('doctors')->where('id', $id)->first();
+        return view('pages.doctors.show', compact('doctor'));
+    }
+
+    //edit doctor
+    public function edit($id)
+    {
+        //$doctor = DB::table('doctors')->where('id', $id)->first();
+        $doctor = Doctor::find($id);
+        return view('pages.doctors.edit', compact('doctor'));
+    }
+
+    //update doctor
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'doctor_name' => 'required',
+            'doctor_specialist' => 'required',
+            'doctor_phone' => 'required',
+            'doctor_email' => 'required|email',
+            'sip' => 'required',
+        ]);
+
+        DB::table('doctors')->where('id', $id)->update([
+            'doctor_name' => $request->doctor_name,
+            'doctor_specialist' => $request->doctor_specialist,
+            'doctor_phone' => $request->doctor_phone,
+            'doctor_email' => $request->doctor_email,
+            'sip' => $request->sip,
+        ]);
+
+        return redirect()->route('doctors.index')->with('success', 'Doctor updated successfully');
+    }
+
+
+    //delete doctor
+    public function destroy($id)
+    {
+        DB::table(('doctors'))->where('id', $id)->delete();
+        return redirect()->route('doctors.index')->with('success', 'Doctor delete successfully');
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Klinik Que</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                    
+                </ul>
+                
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"
+                    data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Master Data</span></a>
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link"
+                            href="{{ route('users.index') }}">Users</a>
+                    </li>
+                    <li class=''>
+                        <a class="nav-link"
+                            href="{{ route('doctors.index') }}">Doctors</a>
+                    </li>
+                </ul>
+            </li>
+           
+        </ul>
+
+        </div>
+    </aside>
+</div>
